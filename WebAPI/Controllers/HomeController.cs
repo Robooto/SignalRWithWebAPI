79769ae9ca4c42dacd2d32753732f8f119c8adad
Robooto <samrussell63@gmail.com>
2015-10-12 01:02:12 -0700

    Initial Commit
    testing Signalr pushing with webapi

---
+++ WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+using WebAPI.Hubs;
+
+namespace WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            var context = GlobalHost.ConnectionManager.GetHubContext<Testing>();
+            context.Clients.All.Send("Hello Everyone");
+            return View();
+        }
+    }
+}
---
+++ WebAPI/Hubs/Testing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebAPI.Hubs
+{
+    public class Testing : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.newMessage(message);
+        }
+
+        public override Task OnConnected()
+        {
+            Debug.WriteLine(Context.ConnectionId);
+            return base.OnConnected();
+        }
+    }
+}
